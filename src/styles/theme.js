616af27fd257f09eb6cad7cba71bb395bd884818
poby123 <youngsm12@naver.com>
2023-01-17 10:25:31 +0900

    init

---
+++ src/styles/theme.js
@@ -0,0 +1,14 @@
+const colors = {
+  mainGreen: "#9FC743",
+  mainLightGreen: "#F8FAED",
+  mainBlack: "#000000",
+  mainGrey: "#E9E9E9",
+  fortGreen: "#9FC743",
+  fontGrey: "#878787",
+  errorRed: "##FF0000",
+  backgroundGrey: "#f2f2f2",
+};
+
+export const theme = {
+  colors,
+};
