31f163ee053236245bf92bbf6c2b575a6df80875
vinivin153 <vinivin153@naver.com>
2023-01-31 18:30:55 +0900

    Fix: 오류 수정 및 스타일 통일

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -15,32 +15,34 @@ article, aside, canvas, details, embed,
 figure, figcaption, footer, header, hgroup,
 menu, nav, output, ruby, section, summary,
 time, mark, audio, video, input, textarea, select {
-	margin: 0;
-	padding: 0;
-	border: 0;
-	font-size: 100%;
-	font: inherit;
-	vertical-align: baseline;
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
 }
+
 article, aside, details, figcaption, figure,
 footer, header, hgroup, menu, nav, section {
-	display: block;
+  display: block;
 }
+
 ol, ul {
-	list-style: none;
+  list-style: none;
 }
+
 blockquote, q {
-	quotes: none;
+  quotes: none;
 }
-blockquote:before, blockquote:after,
-q:before, q: after {
-	content: '';
-	content: none;
+
+blockquote:before, blockquote:after, q:before, q:after {
+  content: none;
 }
 
 table {
-	border-collapse: collapse;
-	border-spacing: 0;
+  border-collapse: collapse;
+  border-spacing: 0;
 }
 
 * {
@@ -48,8 +50,8 @@ table {
 }
 
 html, body {
-	height: 100%;
-	font-family: 'Roboto', sans-serif;
+  height: 100%;
+  font-family: 'Roboto', sans-serif;
 }
 
 body {
@@ -59,21 +61,18 @@ body {
 
 a {
   text-decoration: none;
-	color:inherit;
+  color:inherit;
 }
 
 button {
-	font: inherit;
-	border: none;
+  font: inherit;
+  border: none;
   background: none;
-	font-family: 'Roboto';
+  font-family: 'Roboto';
 }
+
 a, button:hover {
-	cursor: pointer;
-
-button{
-	cursor:pointer;
-
+  cursor: pointer;
 }
 `;
 
