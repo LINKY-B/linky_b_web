b6d74fe63e5ee33b16f1f169977ab2e13ef457fc
vinivin153 <vinivin153@naver.com>
2023-02-01 21:15:16 +0900

    Feat: 상수파일 추가

---
+++ src/constants/constants.js
@@ -0,0 +1,13 @@
+export const SIGNUP_TERMSSERVICE = Object.freeze({
+  TITLE: "이용 약관에 동의해 주세요.",
+  SUBTITLE:
+    "필수 이용 약관에 동의하지 않으면\n서비스 사용에 제한이 있을 수 있습니다.",
+  TERMS1: "서비스 이용 약관",
+  TERMS1_CONTENT: "1111",
+  TERMS2: "개인 정보 처리 방침",
+  TERMS2_CONTENT: "2222",
+  TERMS3: "수집하는 개인정보의 항목",
+  TERMS3_CONTENT: "33333",
+  TERMS4: "개인정보의 보유 및 이용기간",
+  TERMS4_CONTENT: "4444",
+});
