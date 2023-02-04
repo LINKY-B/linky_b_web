ff0c8918bfe64f571d0905611c6157ce5e554558
poby123 <50279318+poby123@users.noreply.github.com>
2023-02-04 14:13:45 +0900

    Feat: Icon 추가

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,17 +1,27 @@
-import { ReactComponent as ThumbUpIcon } from "assets/svgs/thumb_up.svg";
+import { ReactComponent as PlusCircleIcon } from "assets/svgs/plus_circle.svg";
 import { ReactComponent as MinusCircleIcon } from "assets/svgs/minus_circle.svg";
+
 import { ReactComponent as TopArrowCircleIcon } from "assets/svgs/top_arrow_circle.svg";
 import { ReactComponent as LeftArrowIcon } from "assets/svgs/left_arrow.svg";
+import { ReactComponent as RightArrowIcon } from "assets/svgs/right_arrow.svg";
+import { ReactComponent as BottomArrowIcon } from "assets/svgs/bottom_arrow.svg";
+
+import { ReactComponent as ThumbUpIcon } from "assets/svgs/thumb_up.svg";
 import { ReactComponent as DotMenuIcon } from "assets/svgs/dot_menu.svg";
 import { ReactComponent as FilterIcon } from "assets/svgs/filter.svg";
 import { ReactComponent as SearchIcon } from "assets/svgs/search.svg";
-import { ReactComponent as PlusCircleIcon } from "assets/svgs/plus_circle.svg";
-import { ReactComponent as MatchIcon } from "assets/svgs/match.svg";
 import { ReactComponent as CrossIcon } from "assets/svgs/cross.svg";
+import { ReactComponent as CrossFillIcon } from "assets/svgs/cross_fill.svg";
+import { ReactComponent as PinIcon } from "assets/svgs/pin.svg";
+import { ReactComponent as PencilSolidIcon } from "assets/svgs/pencil_solid_circle.svg";
+import { ReactComponent as ChatQuestionMarkIcon } from "assets/svgs/chat_questionmark.svg";
+import { ReactComponent as ProfileQuestionMarkIcon } from "assets/svgs/profile_questionmark.svg";
+import { ReactComponent as QuarterCircleIcon } from "assets/svgs/quarter_circle.svg";
+
 import { ReactComponent as HomeIcon } from "assets/svgs/home.svg";
+import { ReactComponent as MatchIcon } from "assets/svgs/match.svg";
 import { ReactComponent as ChatIcon } from "assets/svgs/chat.svg";
 import { ReactComponent as ProfileIcon } from "assets/svgs/profile.svg";
-import { ReactComponent as PinIcon } from "assets/svgs/pin.svg";
 
 import { CheckCircleIcon } from "./CheckCircleIcon";
 
@@ -20,6 +30,7 @@ export {
   MinusCircleIcon,
   TopArrowCircleIcon,
   LeftArrowIcon,
+  RightArrowIcon,
   CheckCircleIcon,
   DotMenuIcon,
   FilterIcon,
@@ -27,8 +38,14 @@ export {
   PlusCircleIcon,
   MatchIcon,
   CrossIcon,
+  CrossFillIcon,
   HomeIcon,
   ChatIcon,
   ProfileIcon,
   PinIcon,
+  PencilSolidIcon,
+  ChatQuestionMarkIcon,
+  ProfileQuestionMarkIcon,
+  QuarterCircleIcon,
+  BottomArrowIcon,
 };
