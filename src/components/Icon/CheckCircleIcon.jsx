4b2109813bc1760e80f4e636ded7d3f4a884197b
poby123 <50279318+poby123@users.noreply.github.com>
2023-02-06 13:32:46 +0900

    Feat: CheckCircleIcon 크기속성 추가

--- src/components/Icon/CheckCircleIcon.jsx
+++ src/components/Icon/CheckCircleIcon.jsx
@@ -3,28 +3,32 @@ import PropTypes from "prop-types";
 import { ReactComponent as CheckSolidCircleIcon } from "assets/svgs/check_solid_circle.svg";
 import { ReactComponent as CheckFillCircleIcon } from "assets/svgs/check_fill_circle.svg";
 
-const CustomCheckSolidCircleIcon = ({ color }) => {
-  return <CheckSolidCircleIcon fill={color} />;
+const CustomCheckSolidCircleIcon = ({ color, width, height }) => {
+  return <CheckSolidCircleIcon fill={color} width={width} height={height} />;
 };
 
-const CustomCheckFillCircleIcon = ({ color }) => {
-  return <CheckFillCircleIcon fill={color} />;
+const CustomCheckFillCircleIcon = ({ color, width, height }) => {
+  return <CheckFillCircleIcon fill={color} width={width} height={height} />;
 };
 
-const CustomCheckCircleIcon = ({ color, inversed }) => {
+const CustomCheckCircleIcon = ({ color, inversed, width, height }) => {
   const Icon = inversed
     ? CustomCheckFillCircleIcon
     : CustomCheckSolidCircleIcon;
-  return <Icon color={color} />;
+  return <Icon color={color} width={width} height={height} />;
 };
 
 CustomCheckCircleIcon.propTypes = {
   color: PropTypes.string,
   inversed: PropTypes.bool,
+  width: PropTypes.string,
+  height: PropTypes.string,
 };
 
 CustomCheckCircleIcon.defaultProps = {
   color: "#9FC743",
+  width: "1em",
+  height: "1em",
 };
 
 export { CustomCheckCircleIcon as CheckCircleIcon };
