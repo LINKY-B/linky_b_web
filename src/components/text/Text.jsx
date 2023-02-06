c7dfb88b2787426a4c8dbf7c029d725dc30027e8
poby123 <50279318+poby123@users.noreply.github.com>
2023-02-06 13:28:48 +0900

    Feat: Text fontWeight 추가

--- src/components/text/Text.jsx
+++ src/components/text/Text.jsx
@@ -1,9 +1,9 @@
 import PropTypes from "prop-types";
 import { StyledText } from "./Text.style";
 
-export const Text = ({ fontSize, color, children }) => {
+export const Text = ({ fontSize, color, fontWeight, children }) => {
   return (
-    <StyledText fontSize={fontSize} color={color}>
+    <StyledText fontSize={fontSize} fontWeight={fontWeight} color={color}>
       {children}
     </StyledText>
   );
@@ -12,6 +12,7 @@ export const Text = ({ fontSize, color, children }) => {
 Text.propTypes = {
   fontSize: PropTypes.string,
   color: PropTypes.string,
+  fontWeight: PropTypes.number,
 };
 
 export default Text;
