394ccca2a6fdde44049ec791398ff4c2fc74a4dc
vinivin153 <vinivin153@naver.com>
2023-02-13 16:58:46 +0900

    Style: 컴포넌트 이름 변경

--- src/pages/SignUp/TermsService/TermsService.jsx
+++ src/pages/SignUp/TermsService/TermsService.jsx
@@ -2,7 +2,7 @@ import Text from "components/text/Text";
 import SubHeader from "containers/SubHeader/SubHeader";
 import { theme } from "styles/theme";
 import {
-  ContentTitle,
+  ContentBox,
   ContentWrapper,
   StatusBarBase,
   StatusBarStep,
@@ -22,16 +22,17 @@ const TermsService = () => {
       </StatusBarBase>
 
       <ContentWrapper>
-        <ContentTitle>
+        <ContentBox className="Title">
           <Text fontSize={theme.fontSize.md} fontWeight={700}>
             {SIGNUP_TERMSSERVICE.TITLE}
           </Text>
+
           <Spacing />
           <Text fontSize={theme.fontSize.sm}>
             {SIGNUP_TERMSSERVICE.SUBTITLE}
           </Text>
           <Spacing margin={theme.spacing.xl} />
-        </ContentTitle>
+        </ContentBox>
 
         <TermsList>
           <Spacing margin={theme.spacing.xl} />
