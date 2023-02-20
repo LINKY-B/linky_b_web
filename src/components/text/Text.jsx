0bbc10f5fb513211a2299c1cc649bf043b497c50
poby123 <50279318+poby123@users.noreply.github.com>
2023-02-20 22:55:01 +0900

    Style: Text add whitespace props

--- src/components/text/Text.jsx
+++ src/components/text/Text.jsx
@@ -1,9 +1,14 @@
 import PropTypes from "prop-types";
 import { StyledText } from "./Text.style";
 
-export const Text = ({ fontSize, color, fontWeight, children }) => {
+export const Text = ({ fontSize, color, fontWeight, children, whiteSpace }) => {
   return (
-    <StyledText fontSize={fontSize} fontWeight={fontWeight} color={color}>
+    <StyledText
+      fontSize={fontSize}
+      fontWeight={fontWeight}
+      color={color}
+      whiteSpace={whiteSpace}
+    >
       {children}
     </StyledText>
   );
@@ -13,6 +18,7 @@ Text.propTypes = {
   fontSize: PropTypes.string,
   color: PropTypes.string,
   fontWeight: PropTypes.number,
+  whiteSpace: PropTypes.string,
 };
 
 export default Text;
