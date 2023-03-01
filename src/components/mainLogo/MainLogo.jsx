37196d14707b9fb675fc73257eb0399194c86179
ljh0608 <m98m@naver.com>
2023-03-01 18:43:20 +0900

    Feat: 메인로고 최적화

--- src/components/mainLogo/MainLogo.jsx
+++ src/components/mainLogo/MainLogo.jsx
@@ -1,10 +1,11 @@
+import React, { useEffect } from "react";
 import { MainLogoWrapper, MainLogoImg } from "./MainLogo.style";
 import Spacing from "components/spacing/Spacing";
 import Text from "components/text/Text";
 import { useTheme } from "styled-components";
 import PropTypes from "prop-types";
 
-const MainLogo = ({ top }) => {
+const MainLogo = React.memo(({ top }) => {
   const theme = useTheme();
   return (
     <MainLogoWrapper top={top}>
@@ -13,9 +14,9 @@ const MainLogo = ({ top }) => {
       <Text fontSize={theme.fontSize.lg}>마음맞는 선후배 매칭 서비스</Text>
     </MainLogoWrapper>
   );
-};
+});
 
 MainLogo.propTypes = {
-  top: PropTypes.string,
+  top: PropTypes.number,
 };
 export default MainLogo;
