c8592138ed420d3000872f994b05e62f7ca05d64
vinivin153 <vinivin153@naver.com>
2023-03-28 21:18:31 +0900

    Feat: props 추가

--- src/components/inputs/SelectOption.jsx
+++ src/components/inputs/SelectOption.jsx
@@ -4,21 +4,35 @@ import PropTypes from "prop-types";
 /**
  * SelectOption component
  *
- * @param {string} defaultValue 기본 값
+ * @param {string} value 값
+ * @param {string} defaultValue 기본 값(대체문구)
  * @param {string} name 컴포넌트 이름
+ * @param {string} width 넓이 "150px"
  * @param {function} onChange 이벤트 함수
- * @param {Array} options [{value:"재학"},{value:"졸업"},...]과 같은 형식을 갖는 배열
+ * @param {Array} options 배열
  * @returns 스타일 적용된 컴포넌트 반환
  */
-const SelectOption = ({ defaultValue, name, onChange, options }) => {
+const SelectOption = ({
+  value,
+  defaultValue,
+  name,
+  width,
+  onChange,
+  options,
+}) => {
   return (
-    <SelectOptionStyled name={name} onChange={onChange}>
-      <option value="none" defaultValue>
+    <SelectOptionStyled
+      name={name}
+      value={value}
+      width={width}
+      onChange={onChange}
+    >
+      <option value="" defaultValue>
         {defaultValue}
       </option>
-      {options.map((option) => (
-        <option key={option.value} value={option.value}>
-          {option.value}
+      {options.map((item) => (
+        <option key={item} value={item}>
+          {item}
         </option>
       ))}
     </SelectOptionStyled>
@@ -26,10 +40,14 @@ const SelectOption = ({ defaultValue, name, onChange, options }) => {
 };
 
 SelectOption.propTypes = {
+  value: PropTypes.string,
   defaultValue: PropTypes.string,
   name: PropTypes.string,
   onChange: PropTypes.func,
   options: PropTypes.array,
 };
 
+SelectOption.defaultProps = {
+  defaultValue: "기본값을 선택해 주세요",
+};
 export default SelectOption;
--- src/components/inputs/Textarea.jsx
+++ src/components/inputs/Textarea.jsx
@@ -7,17 +7,19 @@ import PropTypes from "prop-types";
  * @param {string} value 입력 값
  * @param {string} width 크기
  * @param {function} onChange 이벤트 함수
+ * @param {function} onBlur 이벤트 함수
  * @param {string} placeholder 대체 입력 문구
  * @returns 스타일 적용된 컴포넌트 반환
  *
  */
-const Textarea = ({ value, width, onChange, placeholder }) => {
+const Textarea = ({ value, width, onChange, onBlur, placeholder }) => {
   return (
     <TextareaStyled
       className="Textarea"
       value={value}
       width={width}
       onChange={onChange}
+      onBlur={onBlur}
       placeholder={placeholder}
     ></TextareaStyled>
   );
@@ -27,6 +29,7 @@ Textarea.propTypes = {
   value: PropTypes.string,
   width: PropTypes.string,
   onChange: PropTypes.func,
+  onBlur: PropTypes.func,
   placeholder: PropTypes.string,
 };
 
