ddd0d80631dbaefb5c2cb2c7dd81a09875f1830f
vinivin153 <vinivin153@naver.com>
2023-04-12 15:32:16 +0900

    Feat: 닉네임 중복 확인 기능 추가

--- src/containers/SignUp/BasicInformation/NickName.jsx
+++ src/containers/SignUp/BasicInformation/NickName.jsx
@@ -2,30 +2,43 @@ import Spacing from "components/spacing/Spacing";
 import Text from "components/text/Text";
 import { useDispatch } from "react-redux";
 import { addUserInfo } from "store/ducks/signUpSlice";
+import { useMutateNickName } from "utils/hooks/useSignUp";
 import InputButtonBox from "../InputButtonBox";
 import { isValidNickName } from "../validator";
 
 const NickName = () => {
   const dispatch = useDispatch("signUp");
+  const mutation = useMutateNickName();
 
-  const handleClick = (inputNickName) => {
-    isValidNickName(inputNickName) ? alert("통과") : alert("실패");
+  const handleClick = async (inputNickName) => {
+    if (isValidNickName(inputNickName)) {
+      const { data } = await mutation.mutateAsync(inputNickName);
 
-    // 성공시
-    dispatch(addUserInfo({ key: "userNickname", value: inputNickName }));
+      if (data.code === "M012") {
+        alert(data.message);
+        return;
+      }
+
+      alert(data.message);
+      dispatch(addUserInfo({ key: "userNickName", value: inputNickName }));
+      return;
+    }
+    alert("올바르지 않은 닉네임 형식입니다.");
   };
 
   return (
-    <InputButtonBox
-      className="user-nickname"
-      title="userNickname"
-      placeholder="닉네임을 입력해주세요.(2~8자)"
-      buttonText="중복 확인"
-      handleClick={handleClick}
-    >
-      <Text>닉네임</Text>
-      <Spacing />
-    </InputButtonBox>
+    <>
+      <InputButtonBox
+        className="user-nickname"
+        title="userNickname"
+        placeholder="닉네임을 입력해주세요.(2~8자)"
+        buttonText="중복 확인"
+        handleClick={handleClick}
+      >
+        <Text>닉네임</Text>
+        <Spacing />
+      </InputButtonBox>
+    </>
   );
 };
 
