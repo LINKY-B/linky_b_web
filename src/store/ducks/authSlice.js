464c0565f8cb8f319e502ca05e926636999e1a05
ljh0608 <m98m@catholic.ac.kr>
2023-04-16 21:06:48 +0900

    Feat: redux-persist 적용

--- src/store/ducks/authSlice.js
+++ src/store/ducks/authSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-// import persistReducer from "redux-persist/es/persistReducer";
+// import { persistReducer } from "redux-persist";
 // import storage from "redux-persist/lib/storage";
 
 // const persistConfig = {
--- src/store/store.js
+++ src/store/store.js
@@ -1,26 +1,44 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { persistStore } from "redux-persist";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import { authReducer } from "./ducks/authSlice";
-import persistedReducer from "./ducks/authSlice";
 import { chatReducer } from "./ducks/chatSlice";
 import { modalReducer } from "./ducks/modalSlice";
-
 import termsServiceReducer from "./ducks/termsServiceSlice";
 import signUpReducer from "./ducks/signUpSlice";
 
-export const store = configureStore({
-  reducer: {
-    modal: modalReducer,
-    termsService: termsServiceReducer,
-    auth: authReducer,
-    // persisted: persistedReducer,
-    chat: chatReducer,
-    signUp: signUpReducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
-  //추후 서버연결시 CORS에러가 난다면 미들웨어 로직 추가 고려
+import thunk from "redux-thunk";
+
+const rootReducer = combineReducers({
+  modal: modalReducer,
+  termsService: termsServiceReducer,
+  auth: authReducer,
+  // persisted: persistedReducer,
+  chat: chatReducer,
+  signUp: signUpReducer,
 });
+export const persistConfig = {
+  key: "root",
+  // localStorage에 저장합니다.
+  storage,
+  // auth, board, studio 3개의 reducer 중에 auth reducer만 localstorage에 저장합니다.
+  whitelist: ["auth"],
+  // blacklist -> 그것만 제외합니다
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+
+  middleware: [thunk],
+
+  // middleware: (getDefaultMiddleware) =>
+  //   getDefaultMiddleware({
+  //     serializableCheck: false,
+  //   }),
+  // 추후 서버연결시 CORS에러가 난다면 미들웨어 로직 추가 고려
+});
+
+export const persistor = persistStore(store);
